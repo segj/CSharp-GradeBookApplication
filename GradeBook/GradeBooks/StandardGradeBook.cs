cd10ad56c0a6fd60c16e06721579e799e6b7bfc1
segj <43644398+segj@users.noreply.github.com>
2018-12-26 12:01:13 +0100

    a

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(String name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
