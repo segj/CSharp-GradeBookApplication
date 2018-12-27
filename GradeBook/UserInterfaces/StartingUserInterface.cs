0c9bf85708faa157fecf106f8b1bb6ad86854c92
segj <43644398+segj@users.noreply.github.com>
2018-12-27 21:28:53 +0100

    y

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,12 +41,12 @@ namespace GradeBook.UserInterfaces
             var name = parts[1];
             BaseGradeBook gradeBook;
 
-            if (parts[3] == "standard")
+            if (parts[2] == "standard")
                 gradeBook = new StandardGradeBook(name);
-            else if (parts[3] == "ranked")
+            else if (parts[2] == "ranked")
                 gradeBook = new RankedGradeBook(name);
             else
-                Console.WriteLine(parts[3] + " is not a supported type of gradebook, please try again"); return;    
+                Console.WriteLine(parts[2] + " is not a supported type of gradebook, please try again"); return;    
 
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
