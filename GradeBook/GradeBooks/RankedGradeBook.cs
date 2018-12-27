f4cb666f8287390579072055f3497f0a41298695
segj <43644398+segj@users.noreply.github.com>
2018-12-27 19:27:11 +0100

    h

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -26,7 +26,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             else
-                base.CalculateStatistics();/*ØSDfsfd*&/*/
+                base.CalculateStatistics();
         }
 
         public override char GetLetterGrade(double averageGrade)
